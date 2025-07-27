4cad9d572f7cca81811fa149b1c36964c8009c2b
TJ Palanca <mail@tjpalanca.com>
2025-07-27 14:09:39 +0000

    fix: copy_readme more tolerant of non-existent readme

--- notes/scripts/copy_readme.py
+++ notes/scripts/copy_readme.py
@@ -2,4 +2,6 @@ import os
 from pathlib import Path
 
 if not os.getenv("GITHUB_ACTIONS"):
-    Path("_site/README.md").rename("../README.md")
+    readme = Path("_site/README.md")
+    if readme.exists():
+        readme.rename("../README.md")
