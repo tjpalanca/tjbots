21cf22febc5984b11536886fd1b41f1754e776f4
TJ Palanca <mail@tjpalanca.com>
2025-07-29 13:06:52 +0000

    feat: add logo to materials

--- notes/scripts/copy_readme.py
+++ notes/scripts/copy_readme.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 if not os.getenv("GITHUB_ACTIONS"):
     readme = Path("_site/README.md")
-    if readme.exists():
+    if readme.is_file():
         readme.rename("../README.md")
